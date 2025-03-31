da43bbebc77e7cb8d917dfd88be8a6e35f258fdf
AmzBG <ameer.boughanem@gmail.com>
2025-04-01 02:39:39 +0300

    Maps Backend: Biggest update here, handling map files (Pre-processing, Sanitizing, customized error handling). This update ensures all required files for the map are available, and customized for future pulls, Fixes #12

---
+++ Maps/backend/src/utils/maps.handler.ts
@@ -0,0 +1,49 @@
+import { FileArray, UploadedFile } from "express-fileupload"
+import { CustomError } from "./Response & Error Handling/custom-error"
+import { toLower } from "lodash"
+import { ensureArray, sanitizePath } from "./Helpers/helper-functions"
+
+export enum FileTypes {
+    MapFile = "mapFile",
+    JsonFile = "jsonFile",
+    TilesetFiles = "tilesetFiles",
+    TemplateFiles = "templateFiles",
+    ImageFiles = "imageFiles"
+}
+
+export const fileTypes = Object.values(FileTypes)
+export const thumbnailFileType = "thumbnailFile"
+export const boothesClassName = "Booths"
+
+export const getRequiredFilesFromJSONFile = (data : Buffer) : { tilesets: Set<string>, templates: Set<string> } => {
+    try {
+        const jsonData = JSON.parse(data.toString())
+
+        const templatesRequired: Set<string> = new Set()
+        const tilesetsRequired: Set<string> = new Set(jsonData.tilesets.map((tileset: any) => sanitizePath(tileset.source)))
+
+        for (const layer of jsonData.layers) {
+            if (layer?.class && toLower(layer.class) === toLower(boothesClassName)) {
+                if (layer.layers) {
+                    for (const subLayer of layer.layers) {
+                        if (subLayer.objects) {
+                            for (const obj of subLayer.objects) {
+                                if (obj.template) {
+                                    templatesRequired.add(obj.template)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return {
+            templates: templatesRequired,
+            tilesets: tilesetsRequired
+        }
+        
+    } catch (err: any) {
+        throw new CustomError("Error gathering required map files", 400)
+    }
+}
