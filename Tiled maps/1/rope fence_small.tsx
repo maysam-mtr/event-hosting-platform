e9008377be6ead5cd7dba4b5e84e3af399814dec
AmzBG <ameer.boughanem@gmail.com>
2025-05-10 04:58:55 +0300

    Tiled maps updated

---
+++ Tiled maps/1/rope fence_small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="rope fence_small" tilewidth="12" tileheight="12" tilecount="56" columns="7">
+ <image source="rope fence_small.png" width="94" height="96"/>
+</tileset>
